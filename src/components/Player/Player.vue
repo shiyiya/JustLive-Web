f4fc053f79d2997fd5772584a57960e0c9024325
shiyiya <shiyiya.11@gmail.com>
2022-12-14 17:18:50 +0800

    -

--- src/components/Player/Player.vue
+++ src/components/Player/Player.vue
@@ -95,7 +95,7 @@ export default {
                     name: `画质`,
                     children: this.quality,
                     onChange({ url }) {
-                      player.changeSource({ src: url })
+                      player.changeQuality({ src: url })
                     }
                   }
                 ]
